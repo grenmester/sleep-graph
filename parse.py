7fcd0216094bed498605694520344fc071e83d2a
Jacky Lee <dr.jackylee@gmail.com>
2017-07-09 03:57:15 -0700

    Format data

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import datetime
 
 sourceFile = "bullet.md"
 dataFile = "output.md"
@@ -7,6 +8,16 @@ with open(sourceFile, "r") as source:
     lines = source.readlines()
 
     for line in lines:
-        if line[0] == "#" or "* sleep" in line or "* nap" in line:
+
+        if line[0] == "#":
             with open(dataFile, "a") as data:
                 data.write(line)
+
+        if "* sleep" in line or "* nap" in line:
+            line = line.replace("* sleep ","").replace("* nap ","")
+            dashPos = line.find("-")
+            firstDate = line[:dashPos]+"\n"
+            secondDate = line[dashPos+2:]
+            with open(dataFile, "a") as data:
+                data.write(firstDate)
+                data.write(secondDate)
