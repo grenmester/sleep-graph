a6c71b2057be0518d01c57973ac5b1a8b97d82fd
Jacky Lee <dr.jackylee@gmail.com>
2017-12-31 01:19:33 -0800

    Put code in separate function

--- parse.py
+++ parse.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 import datetime
 
-sourceFile = "bullet.md"
-dataFile = "output.md"
+def computeIntervals():
+    sourceFile = "bullet.md"
+    dataFile = "output.md"
 
-with open(sourceFile, "r") as source:
-    lines = source.readlines()
+    with open(sourceFile, "r") as source:
+        lines = source.readlines()
 
-    for line in lines:
+        for line in lines:
 
-        if line[0] == "#":
-            with open(dataFile, "a") as data:
-                data.write(line)
+            if line[0] == "#":
+                with open(dataFile, "a") as data:
+                    data.write(line)
 
-        if "* sleep" in line or "* nap" in line:
-            line = line.replace("* sleep ","").replace("* nap ","")
-            dashPos = line.find("-")
-            firstDate = line[:dashPos]+"\n"
-            secondDate = line[dashPos+2:]
-            with open(dataFile, "a") as data:
-                data.write(firstDate)
-                data.write(secondDate)
+            if "* sleep" in line or "* nap" in line:
+                line = line.replace("* sleep ","").replace("* nap ","")
+                dashPos = line.find("-")
+                firstDate = line[:dashPos]+"\n"
+                secondDate = line[dashPos+2:]
+                with open(dataFile, "a") as data:
+                    data.write(firstDate)
+                    data.write(secondDate)
+
