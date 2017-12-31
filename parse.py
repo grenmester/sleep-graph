d14f550fd39a480ddec1fb4dfb45bc63db771a58
Jacky Lee <dr.jackylee@gmail.com>
2017-12-31 01:20:50 -0800

    Add function to compute time spent sleeping every day

--- parse.py
+++ parse.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import datetime
+import dateutil
+import dateutil.parser
 
 def computeIntervals():
     sourceFile = "bullet.md"
@@ -23,3 +25,38 @@ def computeIntervals():
                     data.write(firstDate)
                     data.write(secondDate)
 
+def computeDailyTime():
+    with open("bullet.md", "r") as source:
+        lines = source.readlines()[:160]
+
+        for line in lines:
+            # Identity dates
+            if line[0] == "#":
+                date = line[2:-1]
+            # Identity and parse times
+            elif "* sleep" in line or "* nap" in line:
+                chunks = line.split()
+                time1 = date + " " + chunks[2] + " " + chunks[3]
+                time2 = date + " " + chunks[5] + " " + chunks[6]
+
+                #print("time1: " + str(time1))
+                #print("time2: " + str(time2))
+
+                diff = dateutil.parser.parse(time2) - dateutil.parser.parse(time1)
+                #print("diff: " + str(diff))
+
+                hours = str(diff).split(':')
+                hours = int(hours[0]) * 4 + int(hours[1]) / 15
+                #print("hours: " + str(hours))
+
+                with open("time-output.md", "a") as data:
+                    data.write(time1 + " - " + time2)
+                    data.write('\n')
+                    data.write(str(hours))
+                    data.write('\n')
+
+# TODO: account for periods spanning two days
+# TODO: get list of days with hours asleep
+
+if __name__ == "__main__":
+  computeDailyTime()
