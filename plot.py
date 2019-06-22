fae6f5ca57d70bb6836ba7c65afdf4826b91fbcd
Jacky Lee <dr.jackylee@gmail.com>
2019-06-22 04:09:28 -0700

    Add functionality for generating plot

--- plot.py
+++ plot.py
@@ -1,5 +1,9 @@
+import click
 import datetime as dt
 import json
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import os
 
 
 def extract_sleep_data(json_file, org_file):
@@ -33,3 +37,66 @@ def extract_sleep_data(json_file, org_file):
                 })
         with open(json_file, 'w') as output:
             json.dump(data, output)
+
+
+@click.command()
+@click.argument('json-file', type=click.Path())
+@click.option('-d', '--output-dir', default='output', type=click.Path(),
+              help='Path to output directory.')
+@click.option('-o', '--org-file', default='', type=click.Path(),
+              help='Path to org file with sleep data. If this option is '
+              'provided, a JSON file with the name `JSON_FILE\' will be '
+              'generated and used. If this option is not provided, data will '
+              'be read from `JSON_FILE\'.')
+def gen_plots(json_file, output_dir, org_file):
+    '''
+    Given a JSON file containing sleep data, generate plots analyzing the data.
+    The JSON file can be generated from an org file.
+    '''
+    if org_file:
+        extract_sleep_data(json_file, org_file)
+
+    starts = {}
+    ends = {}
+
+    with open(json_file, 'r') as data:
+        # Load data and convert to datetime objects
+        data = json.load(data)
+        for k, v in data.items():
+            starts[k] = []
+            ends[k] = []
+            for interval in v:
+                start = dt.datetime.strptime(
+                    interval['start'], '%m/%d/%Y %H:%M')
+                end = dt.datetime.strptime(interval['end'], '%m/%d/%Y %H:%M')
+                assert start < end, 'End time is earlier than start time'
+                # Break up intervals so each interval is in one day
+                while start.day < end.day:
+                    temp_end = dt.datetime(
+                        start.year, start.month, start.day, 23, 59)
+                    starts[k].append(start)
+                    ends[k].append(temp_end)
+                    start -= dt.timedelta(days=-1, hours=start.hour,
+                                          minutes=start.minute)
+                starts[k].append(start)
+                ends[k].append(end)
+
+    fig, ax = plt.subplots()
+
+    for k in starts.keys():
+        mpl_starts = mdates.date2num(starts[k])
+        mpl_ends = mdates.date2num(ends[k])
+        start_times = mpl_starts % 1  # Extract time
+        start_days = mpl_starts - start_times
+        durations = mpl_ends - mpl_starts
+        start_times += 1  # So we have a valid date
+
+        ax.bar(start_days, durations, bottom=start_times)
+
+    os.makedirs(output_dir, exist_ok=True)
+    plt.savefig(os.path.join(output_dir, 'graph.png'))
+    plt.show()
+
+
+if __name__ == '__main__':
+    gen_plots()
